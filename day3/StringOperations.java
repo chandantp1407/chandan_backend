478990b527b8166d803c6193003c5c2a24c0267f
chandantp1407 <chandantp1407@gmail.com>
2024-05-06 07:56:18 -0700

    updated

--- day3/StringOperations.java
+++ day3/StringOperations.java
@@ -15,12 +15,12 @@ public class StringOperations {
 		System.out.println(rev);
 		//---------------count words----------------------------
 		int count=countWords(str2);
-		 System.out.println("Number of words in a string is " + count);
-		 
+		System.out.println("Number of words in a string is " + count);
+
 		//-----------------count Vowels-----------------------------
 		int vowels=countVowels(str);
 		System.out.println(vowels);
-		
+
 		//------------------isAllVowels-----------------------------		
 		boolean rs=	isAllVowels(str);
 		if(rs==true) {
@@ -28,6 +28,38 @@ public class StringOperations {
 		}
 		else
 			System.out.println("String is not contains all vowels");
+		//-------------Panagram-----------------------------------
+		boolean rs1=isPanagram(str);
+		if(rs1)
+			System.out.println("String is a panagram");
+		else
+			System.out.println("String is not a panagram");
+		//----------Anagram----------------------------------
+		boolean rs2=isAnagram(str,str2);
+		if(rs2)
+			System.out.println("String is a anagram");
+		else
+			System.out.println("String is not a anagram");
+		//-------------count characters----------
+		countCharacters(str);
+
+	}
+
+	private static void countCharacters(String str) {
+		int [] count=new int[26];
+		for(int i=0;i<str.length();i++)
+		{
+			char ch=str.charAt(i);
+			if(ch>='A'&& ch<='Z')
+				count[ch-65]++;
+			else if(ch>='a'&& ch<='z')
+				count[ch-97]++;
+		}
+		for(int i=0;i<26;i++)
+		{
+			if(count[i]!=0)
+				System.out.println((char)(i+65)+"---->"+count[i]);
+		}
 	}
 
 	private static int countWords(String str2) {
@@ -78,5 +110,48 @@ public class StringOperations {
 	private static boolean isPalindrome(String str) {
 		String rev=reverse(str);
 		return str.equals(rev);
+	}
+	private static boolean isPanagram(String str) {
+		if(str.length()<26)
+			return false;
+		int [] count=new int [26];
+		for(int i=0;i<str.length();i++)
+		{
+			char ch=str.charAt(i);
+			if(ch>='A'&& ch<='Z')
+				count[ch-65]++;
+			else if(ch>='a'&& ch<='z')
+				count[ch-97]++;
+		}
+		for(int i=0;i<26;i++) {
+			if(count[i]==0)
+				return false;
+		}
+
+		return true;
+
+	}
+	private static boolean isAnagram(String str1, String str2) {
+		int [] c1=countFreq(str1);
+		int [] c2=countFreq(str2);
+		for(int i=0;i<26;i++)
+		{
+			if(c1[i]!=c2[i])
+				return false;
+		}
+		return true;
+	}
+
+	private static int[] countFreq(String str) {
+		int [] count=new int[26];
+		for(int i=0;i<str.length();i++)
+		{
+			char ch=str.charAt(i);
+			if(ch>='A'&& ch<='Z')
+				count[ch-65]++;
+			else if(ch>='a'&& ch<='z')
+				count[ch-97]++;
+		}
+		return count;
 	}
 }
